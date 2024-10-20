db96ee56a7eff42c5636fd51eae192613925f97c
Ian Hogers <ianhogers@hotmail.com>
2024-10-20 19:49:52 -0400

    fix[UnpackWritable]: continue unpacking

--- src/types.ts
+++ src/types.ts
@@ -3,5 +3,5 @@ import type { ValueOf } from 'type-fest';
 import type writableWith from '.';
 
 // Gets the value of the writable
-export type UnpackWritable<T> = T extends Writable<infer R> ? R : T;
+export type UnpackWritable<T> = T extends Writable<infer R> ? R : UnpackWritable<T>;
 export type WritableWith = ValueOf<typeof writableWith> | typeof writable;
